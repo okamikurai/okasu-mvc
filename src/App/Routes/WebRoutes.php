9ab602ca13aec4211d1013564073dab97a73b438
Okami Kurai <okami.kurai.ryu@gmail.com>
2024-10-18 13:06:09 -0600

    Inicio de session con Db y estructura inicial

--- src/App/Routes/WebRoutes.php
+++ src/App/Routes/WebRoutes.php
@@ -17,11 +17,12 @@ class WebRoutes {
         $router->addRoute('GET', '/home', [HomeController::class, 'home']);
 
         $router->addRoute('GET', '/login', [HomeController::class, 'login']);
-        $router->addRoute('GET', '/logout', [HomeController::class, 'login']);
-        $router->addRoute('GET', '/msHomeUser', [HomeController::class, 'msHomeUser'], 'auth');
+        $router->addRoute('GET', '/HomeUser', [HomeController::class, 'msHomeUser'], 'auth');
+
 
         $router->addRoute('GET', '/auth', [AuthController::class, 'authMsGraph']);
-        $router->addRoute('GET', '/mslogout', [AuthController::class, 'logout']);
+        $router->addRoute('GET', '/logout', [AuthController::class, 'logout']);
+        $router->addRoute('POST', '/auth', [AuthController::class, 'authLocal']);
 
         return $router;
     }
